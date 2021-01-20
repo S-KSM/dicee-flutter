bf17f6d6d1b5665ac367d4c54e5ecf996a22408e
Shobeir K. S. Mazinani <shobeir.mazinani@gmail.com>
2021-01-19 19:24:52 -0500

    final commit of the dicee

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+
+void randomize() {
+  return Random().nextInt(6) + 1;
+}
 
 void main() {
   return runApp(
@@ -6,7 +12,9 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(
+            child: Text('Dicee'),
+          ),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +23,52 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Container(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    rightDiceNumber = randomize();
+                    leftDiceNumber = randomize();
+                    print(
+                        'Right Button is pressed. New Value = $leftDiceNumber');
+                  });
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    rightDiceNumber = randomize();
+                    leftDiceNumber = randomize();
+                  });
+                  print(
+                      'Right Button is pressed. New Value = $rightDiceNumber');
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
